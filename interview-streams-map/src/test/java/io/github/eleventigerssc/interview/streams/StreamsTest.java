6be6bee853495e3fe53dff055aeb08337e52ce65
jokubasdargissc <jokubas.dargis@snapchat.com>
2016-12-15 14:34:15 -0800

    Add few comments to the test

--- interview-streams-map/src/test/java/io/github/eleventigerssc/interview/streams/StreamsTest.java
+++ interview-streams-map/src/test/java/io/github/eleventigerssc/interview/streams/StreamsTest.java
@@ -30,20 +30,29 @@ public class StreamsTest {
 
     @Test
     public void map_flatMap_forEach() {
+        // Create a stream from an iterable of Strings
         Stream<String> stream = Streams.from(strings);
+        // strings object should not be touched at this point, think lazy
         verifyZeroInteractions(strings);
 
+        // Create a mapped stream of Strings that are uppercase
         Stream<String> upperCaseStrings = stream.map(UPPERCASE);
+        // Mapping should be a lazy operation, don't touch the string object yet
         verifyZeroInteractions(strings);
 
+        // Decompose(flatMap) the uppercase strings stream into individual character stream
         Stream<Character> characterStream = upperCaseStrings.flatMap(CHARACTERS);
+        // flatMap is lazy too
         verifyZeroInteractions(strings);
 
+        // Just a test util
         InOrder inOrder = inOrder(logger);
 
+        // We finally use the result stream here, initial strings iterable may be touched
         characterStream.forEach(logger::log);
         inOrder.verify(logger, calls(12)).log(anyChar());
 
+        // Let's try iterate again to see if forEach is stateless
         characterStream.forEach(logger::log);
         inOrder.verify(logger, calls(12)).log(anyChar());
     }
