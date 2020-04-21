c9df757ca8aec3fe059ebc8f9e7c518a06394119
jokubas-dargis <jokubas.dargis@snapchat.com>
2020-04-21 12:18:04 -0700

    [morse] Add a helper map

--- morse-coder/src/main/java/io/github/eleventigerssc/interview/morse/MorseCoders.java
+++ morse-coder/src/main/java/io/github/eleventigerssc/interview/morse/MorseCoders.java
@@ -1,5 +1,8 @@
 package io.github.eleventigerssc.interview.morse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 final class MorseCoders {
 
     /**
@@ -8,4 +11,53 @@ final class MorseCoders {
     static MorseCoder get() {
         return MorseCoder.NOOP;
     }
+
+    /**
+     * @return map of characters to Morse encoded strings that can be used to implement {@link MorseCoder}.
+     */
+    static Map<Character, String> charToMorse() {
+        return CHARS_T0_MORSE;
+    }
+
+    private static final Map<Character, String> CHARS_T0_MORSE = new HashMap<Character, String>() {{
+        put('a', ".-");
+        put('b', "-...");
+        put('c', "-.-.");
+        put('d', "-..");
+        put('e', ".");
+        put('f', "..-.");
+        put('g', "--.");
+        put('h', "....");
+        put('i', "..");
+        put('j', ".---");
+        put('k', "-.-");
+        put('l', ".-..");
+        put('m', "--");
+        put('n', "-.");
+        put('o', "---");
+        put('p', ".--.");
+        put('q', "--.-");
+        put('r', ".-.");
+        put('s', "...");
+        put('t', "-");
+        put('u', "..-");
+        put('v', "...-");
+        put('w', ".--");
+        put('x', "-..-");
+        put('y', "-.--");
+        put('z', "--..");
+        put('1', ".----");
+        put('2', "..---");
+        put('3', "...--");
+        put('4', "....-");
+        put('5', ".....");
+        put('6', "-....");
+        put('7', "--...");
+        put('8', "---..");
+        put('9', "----.");
+        put('0', "-----");
+        put('?', "··--··");
+        put(',', "--··--");
+        put('.', "·-·-·-");
+    }};
 }
